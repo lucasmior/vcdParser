387bc6d3a37a5e6b5d198fa1b8e159c1d9bf3beb
lucas.mior <lucas.mior@gmail.com>
2016-04-27 01:47:15 -0300

    Changed the Parser

--- vcd.c
+++ vcd.c
@@ -1,8 +1,26 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "uthash.h"
 
-char* getWord(FILE* vcd);
-main()
+char* getLine(FILE* vcd);
+char* getWord(char* line);
+
+
+struct my_struct {
+	int id;
+    int frequence;            /* we'll use this field as the key */
+    char module[30];             
+    UT_hash_handle hh; /* makes this structure hashable */
+};
+
+struct my_struct *users = NULL;
+
+void add_user(struct my_struct *s) {
+    HASH_ADD_INT( users, id, s );    
+}
+
+int main( )
 {
 	FILE *vcd;
 	vcd = fopen("test.vcd", "rt");
@@ -12,52 +30,71 @@ main()
 		return -1;
 	} 
 	
-	char word[100];
-	char start[] = "$scope";
+	char *line = malloc(sizeof(char)*100);
+	char *word = malloc(sizeof(char)*100);
+	char *module = malloc(sizeof(char)*30);
+	const char end[] = "$dump";
+	const char s[2] = " ";
 	
-	printf("Reading header\n");
+	printf("Getting variables\n");
 	do{
-		strcpy(word, getWord(vcd));
-		printf("%s\n",word );
-	}while( strcmp(word, start) );
-	
-	printf("Reading $var\n");
-	int ret = 0;
-	do{
-		strcpy(word, getWord(vcd));
-		printf("OPA\n");
-		if( !strcmp(word, (char*)"$var") )
+		strcpy(line, getLine(vcd));
+		strcpy(word, getWord(line));
+		if( !strcmp(word, (char*)"$scope") )
 		{
-			printf("HELLO\n");
-			//;//add here
+			strtok(NULL, s);
+			module = strtok(NULL, s);
+			printf("Scope:: %s\n", module);
 		}
-		printf("WORLD\n");
-		else ret = 1; //break
-	}while(!ret);
+		else if( !strcmp(word, (char*)"$var") )
+		{
+			strtok(NULL, s);
+			strtok(NULL, s);
+			word = strtok(NULL, s);
+			printf("Var:: %s\n", word);
+			struct my_struct* s;
+			//s->frequence = 0;
+			//strcpy(s->module, module);
+			//stdcpy(s.key, word);
+			//s->id = 1;
+			//add_user(s); 
+		}
+		//else skip
+	}while( strcmp(word, end) );
 	
+	printf("Counting occurences\n");
+	//TODO
+	printf("Showing results\n");
+	//TODO
+
+	return 0;
 }
 
-char* getWord(FILE* vcd)
+char* getWord(char* line)
 {
-	char line[100];
 	char *word;
-	//printf("Get word\n");
+	char *l = malloc(sizeof(char)*100);
+	const char s[2] = " ";
+
+	if(line != NULL)
+	{
+		strcpy(l, line);
+		word = strtok(l, s);
+		// TODO Verify if it is needed.
+		if(word == NULL)
+		{
+			strcpy(word, l);
+		}
+		return (char*)word;
+	}
+	return NULL;
+}
+char* getLine(FILE* vcd)
+{
+	char *line = malloc(sizeof(char)*100);
 	if(fgets(line, 100, vcd) != NULL)
 	{
-		const char s[2] = " ";
-		//printf("line: %s\n", line);
-		word = strtok(line, s);
-		if (word == NULL)
-		{
-			//printf("here:\n");
-			strcpy(word, line);
-			return (char*)word;
-		}
-		else{
-			//printf("word: %s \n", word);
-			return (char*)word;
-		}
+		return line;
 	}
-	return (char*)'c';
+	return NULL;
 }
-
